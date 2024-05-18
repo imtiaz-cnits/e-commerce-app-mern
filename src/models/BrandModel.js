2dc82c4a0ead84cad03f44646f2a1742bf46474c
imtiaz-cnits <ia64744@gmail.com>
2024-05-18 19:37:19 +0600

    working on models code

--- src/models/BrandModel.js
+++ src/models/BrandModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const DataSchema = mongoose.Schema(
   {
-    brandName: { type: String, unique: true },
+    brandName: { type: String, unique: true, required: true },
     brandImg: { type: String, unique: true },
   },
   { timestamps: true, versionKey: false }
