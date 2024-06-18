7d028dea91d52c70f41b73518d079da3f3188cc8
imtiaz-cnits <ia64744@gmail.com>
2024-06-18 23:41:08 +0600

    api setting

--- src/routes/api.js
+++ src/routes/api.js
@@ -4,5 +4,11 @@ const router = express.Router();
 
 // Products
 router.get("/ProductBrandList", ProductController.ProductBrandList);
+router.get("/ProductCategoryList", ProductController.ProductCategoryList);
+router.get("/ProductSliderList", ProductController.ProductSliderList);
+router.get(
+  "/ProductListByBrand/:BrandID",
+  ProductController.ProductListByBrand
+);
 
 module.exports = router;
