5657d1c8199f423fba93ee641b13fc479cbbafdc
imtiaz-cnits <ia64744@gmail.com>
2024-11-05 04:15:29 +0600

    skeleton ongoing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,7 @@ import 'bootstrap'
 import './assets/css/main.css'
 import './assets/css/animate.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- client/src/pages/home-page.jsx
+++ client/src/pages/home-page.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Layout from "../components/layout/layout.jsx";
+import SliderSkeleton from "../skeleton/slider-skeleton.jsx";
 
 const HomePage = () => {
     return (
         <Layout>
-            
+            <SliderSkeleton/>
         </Layout>
     );
 };
