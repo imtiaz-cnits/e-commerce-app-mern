6a2cbef80d795dfe6e08d21dc548bb482e3cd1ee
imtiaz-cnits <ia64744@gmail.com>
2025-01-27 03:19:12 +0600

    slider components work ongoing

--- client/src/components/product/brands.jsx
+++ client/src/components/product/brands.jsx
@@ -24,7 +24,7 @@ const Brands = () => {
                             BrandList.map((item, i) => {
                                 return (
                                     <div key={i} className="col-6 col-lg-8r text-center col-md-8r p-2">
-                                        <Link to="" className="card h-100 rounded-3 bg-white">
+                                        <Link to={`/by-brand/${item['_id']}}`} className="card h-100 rounded-3 bg-white">
                                             <div className="card-body">
                                                 <img alt={'img'} className="w-75" src={item['brandImg']}/>
                                                 <p className="bodySmal mt-3">{item['brandName']}</p>
